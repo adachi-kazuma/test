ddcb6520f1c0b0cb6bdc975d5c0ee448b3717e97
k022g <k022g@MSI>
2024-10-23 12:21:19 +0900

    されに変更

--- test.cpp
+++ test.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
 
-    std::cout << "Hello New branch changed\n";
+    std::cout << "Hello New branch chenged\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
